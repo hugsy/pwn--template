433f64522dcf5c62c3a36ae8edc55f82a4d594ad
crazy hugsy <hugsy@users.noreply.github.com>
2024-10-25 14:39:17 -0700

    Add Windows DLL template  (#4)
    * Update main.cpp
    
    * add `PWN_BUILD_CRYPTO` to cmake file
    
    * add using namesapce

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,11 @@
 #include <pwn>
+using namespace pwn;
 
-#if 0
-BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved)
-#else
-auto wmain(const int argc, const wchar_t* argv) -> int
-#endif
+// uncomment below if cmake was configured with BUILD_SHARED_LIBS
+// BOOL WINAPI DllMain(HINSTANCE hinstDLL, DWORD fdwReason, LPVOID lpvReserved) 
+
+auto 
+wmain(const int argc, const wchar_t** argv) -> int
 {
     ok(L"hack the planet 🌍!!");
     return 0;
